2ea1a0d191ecb267324dfa2b44aad534fad196a0
Luis Felipe Maia <lf.mcosta23@gmail.com>
2026-01-25 21:54:25 -0300

    Criação do banco de dados e tabelas, conexão com o banco e classe de configuração.

---
+++ src/main/resources/database/create-database-and-tables.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE sbatch_catalog;
+USE sbatch_catalog;
+
+CREATE TABLE TbEletronico (
+    id INT UNSIGNED PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    taxa DECIMAL(3,2) NOT NULL,
+    dataCriacao DATETIME NOT NULL
+);
+
+CREATE TABLE TbLivro (
+    id INT UNSIGNED PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    isbn VARCHAR(16) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    dataCriacao DATETIME NOT NULL
+);
+
+CREATE TABLE TbRoupa (
+    id INT UNSIGNED PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    tamanho VARCHAR(3) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    dataCriacao DATETIME NOT NULL
+);
